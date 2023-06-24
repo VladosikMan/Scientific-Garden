d0e0e6e81611327978b8973728ff179f0d13dd84
VladGad <vladgadved18@mail.ru>
2023-06-24 19:38:31 +0300

    project: add data class  for general data

---
+++ api_json.py
@@ -0,0 +1,9 @@
+class Scientific_api(object):
+    def __init__(self):
+        # дата класс описания общего апи, информация общая для каждого ресурса, когда переход на отдельный файл
+        self.title = "Заголовок"
+        self.summary = "Описание"
+        self.authors = "Авторы"
+        self.type = "Тип"
+        self.link = "Ссылка"
+        self.data 
